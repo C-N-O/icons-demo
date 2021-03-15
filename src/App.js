55da8a67fc30501c533fbb50ee0fa43bfc78a81c
C-N-O <meetmancliff@yahoo.com>
2021-03-14 23:43:25 -0500

    icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { IconContext } from 'react-icons'; //to set value props across all icons
+import { FaBattleNet } from 'react-icons/fa'; //import from font awesome
+import { MdAlarm } from 'react-icons/md'; //import from material design
+import { SiFifa } from 'react-icons/si'; //import from simple icon
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IconContext.Provider value={{ color: 'blue', size: '5rem' }}>
+      <div className='App'>
+        <FaBattleNet color='purple' size='20rem' />
+        <MdAlarm color='green' size='10rem' />
+        <SiFifa />
+      </div>
+    </IconContext.Provider>
   );
 }
 
 export default App;
+
+// To use react icons, I used: npm install react-icons
